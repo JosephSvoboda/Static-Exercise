6f72b371a7abaceea3b443b86d716a7e77d349ac
Joseph Svoboda <joseph.svoboda15@yahoo.com>
2022-11-01 14:32:20 -0400

    added a new temperature class to convert f to c and c to f

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(2));
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(35.6));
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public class TempConverter
+    {
+        public static double FahrenheitToCelsius(double myInt)
+        {
+            double celcius;
+            celcius = (myInt - 32) * 5 / 9;
+            return celcius;
+        }
+
+        public static double CelsiusToFahrenheit(double myInt)
+        {
+            double fahrenheit;
+            fahrenheit = myInt * 1.8 + 32;
+            return fahrenheit;
+        }
+    }
+}
